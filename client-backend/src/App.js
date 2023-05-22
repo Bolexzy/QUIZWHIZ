452d48a94923eff77db4f6348047325acecd8830
Bolexzy <boluoyewumi79@gmail.com>
2023-05-22 03:41:34 +0100

    Update: added features for landing page

--- client-backend/src/App.js
+++ client-backend/src/App.js
@@ -1,39 +1,45 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  theme,
-} from '@chakra-ui/react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { Text, Center } from '@chakra-ui/react';
-
+import './App.css';
 import Root from './components/Root';
 import QuizSettingPage from './components/quiz_settings_components/QuizSettingPage';
-
-import './index.css';
+import LandingPage from './components/landing_page/LandingPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root />,
+    path: '/',
+    // modified to landing page
+    element: <LandingPage />,
     children: [
       {
         index: true,
-        element: <Center><Text>Home page</Text></Center>,
+        element: (
+          <Center>
+            <Text>Home page</Text>
+          </Center>
+        ),
       },
       {
-        path: "/setting",
-        element: <Center><Text>Setting page</Text></Center>,
+        path: '/setting',
+        element: (
+          <Center>
+            <Text>Setting page</Text>
+          </Center>
+        ),
       },
       {
-        path: "/takequiz",
-        element:<Center><Text>I dont know what this is supposed to be</Text></Center>,
+        path: '/takequiz',
+        element: (
+          <Center>
+            <Text>I dont know what this is supposed to be</Text>
+          </Center>
+        ),
       },
       {
-        path: "/setquiz",
+        path: '/setquiz',
         element: <QuizSettingPage />,
       },
     ],
