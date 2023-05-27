e2df23887c836a6f7cb1ecaac1da549d4cd08f1a
Stanley Ezechukwu <stanley6453@gmail.com>
2023-05-27 16:36:33 +0100

    added functionality to the quiz details form

--- client-backend/src/components/quiz_settings_components/QuizSettingPage.js
+++ client-backend/src/components/quiz_settings_components/QuizSettingPage.js
@@ -2,31 +2,76 @@ import React, { useState, useRef } from 'react';
 import { Button, Flex, Box, Text } from '@chakra-ui/react';
 import { v4 } from "uuid";
 import { useParams } from 'react-router-dom';
+import { auth } from '../firebase__init_scripts/firebaseAppInit';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import DisplayQuestions from './DisplayQuestions';
 import questions from '../../set_quiz_questions.json';
 import QuizDetails from './QuizDetails';
 import AIQuestionGenerator from './ai_question_generator/AIQuestionGenerator';
 
+const axios = require('axios');
+
 
 export default function QuizSettingPage() {
-    const [formValues, setFormValues] = useState({
-        title: '',
-        description: '',
-        alloted_time_in_mins: '',
-        quiz_start_datetime: '',
-        quiz_end_datetime: ''
-      });
+    const [user, loading, error] = useAuthState(auth);
     const [quiz, setQuiz] = useState(questions);
     const [toggleCloseAll, setToggleCloseAll] = useState(true);
     const [render, toggleRender] = useState(false);
     const addButton = useRef();
     const closeOptionsRef = useRef([]);
+
     //for getting url parameters
     const params = useParams();
 
     //quizId
     const quizId = params.quizId;
+    console.log('quizid: ')
     console.log(quizId)
+
+
+
+    //Manage QuizDetails Component State
+    const [formValues, setFormValues] = useState({
+        title: '',
+        description: '',
+        alloted_time_in_mins: '',
+        quiz_start_time: '',
+        quiz_end_time: ''
+    });
+
+    const handleQuizDetailsChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            [name]: value
+        }));
+        console.log('form values')
+        console.log(formValues);
+    };
+
+    const handleQuziDetailsFormSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('userid: ', user.uid);
+        let constantQuizData = {
+            "test_id": quizId ?? v4(),
+            "user_id": user.uid,
+        }
+
+        let quizData = { ...constantQuizData, ...formValues };
+
+        quizData.questions = quiz;
+
+        console.log('quiz data to sned:', quizData);
+        console.log('submit')
+
+        axios.post('https://quizwhiz.onrender.com', {
+            firstName: 'Fred',
+            lastName: 'Flintstone'
+        })
+    };
+
+
 
     const createNewQuestionObject = () => {
         setQuiz((prevState) => [...prevState, {
@@ -49,7 +94,7 @@ export default function QuizSettingPage() {
         setTimeout(() => { addButton.current.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' }) }, 1000)
     }
 
-    const appendQuestionToQuiz = (questionObject) =>{
+    const appendQuestionToQuiz = (questionObject) => {
         setQuiz((prevState) => [...prevState, questionObject]);
         setTimeout(() => { addButton.current.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'start' }) }, 1000)
     }
@@ -133,9 +178,10 @@ export default function QuizSettingPage() {
 
     return (
         <Box maxW='85vw' p='3px' boxSizing='boarder-box' pos='relative' mr='auto' ml='auto'>
-            {console.log(quiz)}
+            {console.log('quiz: ', quiz)}
+            {console.log('user:', user)}
             <Box>
-            <QuizDetails formValues={formValues} setFormValues={setFormValues}  />
+                <QuizDetails formValues={formValues} handleQuizDetailsChange={handleQuizDetailsChange} handleQuziDetailsFormSubmit={handleQuziDetailsFormSubmit} />
             </Box>
             <Flex justifyContent="space-around" h='100%'>
                 <Box flex='3' position='relative'>
